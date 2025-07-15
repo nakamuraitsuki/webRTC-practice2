fc61ed675a9f74b95b8075692a1a8d1c1a8e847a
nakamuraitsuki <isukin@gmail.com>
2025-07-15 14:51:51 +0900

    feat: ProviderによるUseCaseの共有

---
+++ frontend/src/app/providers/RoomProvider.tsx
@@ -0,0 +1,48 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { Room } from "../../domains/room/models/Room";
+import { createRoomUseCase, RoomUseCase } from "../../domains/room/usecase/RoomUseCase";
+import { createRoomRepository } from "../../infrastructure/api/RoomRepositoryImpl";
+
+type RoomContextValue = {
+  rooms: Room[] | null;
+  usecase: RoomUseCase
+}
+
+export const RoomContext = createContext<RoomContextValue | undefined>(undefined);
+
+export const RoomProvider = ({ children }: { children: ReactNode}) => {
+  const [rooms, setRooms] = useState<Room[] | null>(null);
+
+  const roomRepo = createRoomRepository();
+  const baseUsecase = createRoomUseCase(roomRepo);
+
+  // UseCaseがステートに関われるようにラップ
+  const usecase: RoomUseCase = {
+    async createRoom(input) {
+      await baseUsecase.createRoom(input);
+      // ルーム作成後に全ルームを再取得
+      const allRooms = await baseUsecase.getAllRooms();
+      setRooms(allRooms);
+    },
+    async getAllRooms() {
+      const allRooms = await baseUsecase.getAllRooms();
+      setRooms(allRooms);
+      return allRooms;
+    }
+  };
+
+  // 初回ルーム取得
+  useEffect(() => {
+    baseUsecase.getAllRooms()
+      .then(setRooms)
+      .catch(() => {
+        setRooms([]);
+      });
+  }, []);
+  
+  return (
+    <RoomContext.Provider value={{ rooms, usecase } as RoomContextValue}>
+      {children}
+    </RoomContext.Provider>
+  )
+}
