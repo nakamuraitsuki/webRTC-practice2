3c5d37fd896fbc106dd938e2e6adaeb9507d79f7
nakamuraitsuki <isukin@gmail.com>
2025-07-15 14:23:28 +0900

    feat: roomのドメインロジック

---
+++ frontend/src/domains/room/usecase/RoomUseCase.ts
@@ -0,0 +1,18 @@
+import { Room } from "../models/Room";
+import { CreateRoomInput, RoomRepository } from "../repositories/RoomRepository";
+
+export interface RoomUseCase {
+  createRoom: (input:CreateRoomInput) => Promise<void>;
+  getAllRooms: () => Promise<Room[]>;
+}
+
+export const createRoomUseCase = (repo: RoomRepository): RoomUseCase => {
+  return {
+    createRoom: async (input: CreateRoomInput) => {
+      await repo.create(input);
+    },
+    getAllRooms: async () => {
+      return await repo.getAll();
+    }
+  };
+}
