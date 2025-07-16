41c0f2dc9754776d268d950760ca309a264f0a8d
nakamuraitsuki <ituki1911n@gmail.com>
2025-07-16 23:38:13 +0900

    feat: add repository Impl to Text Message History

---
+++ frontend/src/infrastructure/api/TextMessageHistoryRepositoryImpl.ts
@@ -0,0 +1,13 @@
+import { GetHistoryInput, GetHistoryOutput } from "../../domains/message/repotisories/TextMessageHistoryRepository"
+import { apiClient } from "./apiClient"
+
+export const createTextMessageHistoryRepository = () => {
+  return {
+    // Comment の履歴取得
+    async getMessageHistory(input: GetHistoryInput): Promise<GetHistoryOutput> {
+      return await apiClient.get<GetHistoryOutput>(
+        `/api/message/${input.roomId}?limit=${input.limit}&before_sent_at=${input.beforeSentAt}`
+      );
+    }
+  }
+}
