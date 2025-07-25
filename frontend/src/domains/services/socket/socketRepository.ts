f966fff383be85f7c69737ca6b2978b487ff8b98
nakamuraitsuki <isukin@gmail.com>
2025-07-25 13:28:02 +0900

    feat: add socket send/recv interface

---
+++ frontend/src/domains/services/socket/socketRepository.ts
@@ -0,0 +1,11 @@
+// Socket通信の抽象化インターフェース
+
+import { Message } from "../../message/models/Message";
+
+export interface SocketRepository {
+  connect(): void; // 接続開始
+  disconnect(): void; // 接続終了
+  send(message: Message): void; // メッセージ送信
+  onMessage(callback: (message: Message) => void): void; // メッセージ受信のコールバック登録
+  onError(callback: (error: Error) => void): void; // エラー発生時のコールバック登録
+}
