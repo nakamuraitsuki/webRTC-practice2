ae6987c2809b91836e0bdc1e17c4dc3327ed8cfd
nakamuraitsuki <isukin@gmail.com>
2025-07-25 16:03:55 +0900

    feat: add socketClient class

---
+++ frontend/src/infrastructure/socket/socketClient.ts
@@ -0,0 +1,70 @@
+import { Message } from "../../domains/message/models/Message";
+
+export type URLInput = {
+  baseUrl: string;
+  endpoint: string;
+}
+
+export class SocketClient {
+  private socket: WebSocket | null = null;
+  private readonly baseUrl: string = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+  private readonly url: string;
+  
+  // イベントリスナーを管理するMap
+  // TODO: 現在は(any => void)だが、UseCaseが定まったら型を厳密にする
+  private listeners: Map<string, (data: any) => void> = new Map();
+
+  constructor(endpoint: string) {
+    this.url = this.baseUrl+ endpoint;
+  }
+
+  connect() {
+    if (this.socket) {
+      console.warn("WebSocket is already connected.");
+      return;
+    }
+    
+    this.socket = new WebSocket(this.url);
+
+    // 生成したSocketに対してイベントリスナーを設定
+    this.socket.onmessage = (event) => {
+      // JSONパース
+      const parsed = JSON.parse(event.data);
+      // 必ず汎用型で送られてくる
+      const message = parsed as Message;
+      // 対応ハンドラをMapから取得
+      const handler = this.listeners.get(message.message_type);
+      // ペイロードを受け渡し
+      handler?.(message.payload);
+    };
+
+  }
+
+  // イベントリスナーを登録
+  // TODO：型について後で吟味
+  on<T>(messageType: string, handler: (data: T) => void) {
+    if (!this.socket) {
+      console.warn("WebSocket is not connected.");
+      return;
+    }
+    // Mapに登録
+    this.listeners.set(messageType, handler as (data: any) => void);
+  }
+
+  // 汎用送信関数
+  send(data: any) {
+    if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
+      console.warn("Socket is not open.");
+      return;
+    }
+    // JSON形式で送る
+    this.socket.send(JSON.stringify(data));
+  }
+
+  disconnect() {
+    if (!this.socket) return;
+
+    this.socket.close();
+    this.socket = null;
+  }
+}
