69c825b7ff7b3207fb4bbe4c5552abcb9af6cb1b
nakamuraitsuki <isukin@gmail.com>
2025-08-17 10:40:04 +0900

    feat: add TextMessageLiveUseCase interface and factory

--- frontend/src/domains/TextMessage/usecase/TextMessageLiveUseCase.ts
+++ frontend/src/domains/TextMessage/usecase/TextMessageLiveUseCase.ts
@@ -1,11 +1,34 @@
+import { Message } from "../../message/models/Message";
+import { SocketService } from "../../services/socket/socketService";
 import { TextMessage } from "../models/TextMessage";
+
+export type SendTextMessageInput = {
+  room_id: string; // ルームID
+  user_id: string; // ユーザーID
+  content: string; // メッセージ内容
+};
 
 export interface TextMessageLiveUseCase {
   // メッセージの送信
-  sendMessage: (roomId: string, userId: string, content: string) => Promise<void>;
+  sendMessage: (input: SendTextMessageInput) => Promise<void>;
 
   // メッセージの受信
   receiveMessage: (payload: TextMessage) => Promise<void>;
 }
 
-export const createTextMessageLiveUseCase = (socket: 
+export const createTextMessageLiveUseCase = (socket: SocketService): TextMessageLiveUseCase => {
+  return {
+    sendMessage: async (input: SendTextMessageInput) => {
+      const message: Message = {
+        message_type: 'text',
+        payload: input as TextMessage
+      }
+
+      socket.send(message);
+    },
+
+    receiveMessage: async (_payload: TextMessage): Promise<void> => {
+      // NOTE: Providerで状態更新関数を注入する前提のエントリポイント
+    }
+  }
+}
