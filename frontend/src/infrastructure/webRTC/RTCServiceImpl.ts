442f02a8d44b3119491cadd6bfec58b2805cd9bb
nakamuraitsuki <ituki1911n@gmail.com>
2025-09-22 23:06:55 +0900

    feat: add RTCService factory function

---
+++ frontend/src/infrastructure/webRTC/RTCServiceImpl.ts
@@ -0,0 +1,14 @@
+import { RTCService } from "../../domains/services/rtcService";
+import { RTCClient } from "./RTCClient";
+
+export const createRTCService = (): RTCService => {
+  const rtcClient = new RTCClient();
+  return {
+    createOffer: () => rtcClient.createOffer(),
+    respondToOffer: (offer) => rtcClient.createAnswer(offer),
+    applyRemoteAnswer: (answer) => rtcClient.setRemoteDescription(answer),
+    addRemoteIceCandidate: (candidate) => rtcClient.addIceCandidate(candidate),
+    addIceCandidateCallback: (callback) => rtcClient.addIceCandidateCallback(callback),
+    closeConnection: () => rtcClient.closeConnection(),
+  }
+}
